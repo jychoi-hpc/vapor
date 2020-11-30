9268bff3c56cfa2c0d9389c078a084665fbc2d6f
Choi <choij@ornl.gov>
2020-11-30 09:56:56 -0500

    add noise

--- vapor.py
+++ vapor.py
@@ -863,6 +863,7 @@ def main():
     parser.add_argument('--fieldline', help='fieldline', action='store_true')
     parser.add_argument('--overwrite', help='overwrite', action='store_true')
     parser.add_argument('--log', help='log', action='store_true')
+    parser.add_argument('--addnoise', help='addnoise', action='store_true')
     args = parser.parse_args()
 
     DIR=args.wdir
@@ -1069,11 +1070,13 @@ def main():
     num_training_updates=args.num_training_updates
     logging.info ('Training: %d' % num_training_updates)
     model.train()
+    ns = 0.0 
     t0 = time.time()
     for i in xrange(istart, istart+num_training_updates):
         (data, lb) = next(iter(training_loader))
         # print ("Training:", lb)
-        ns = torch.Tensor(np.random.normal(0.0, data.numpy()*0.1, size=data.numpy().shape)).to(device)
+        if args.addnoise:
+            ns = torch.Tensor(np.random.normal(0.0, data.numpy()*0.1, size=data.numpy().shape)).to(device)
         data = data.to(device)
         optimizer.zero_grad() # clear previous gradients
         
